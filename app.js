be00b6f602bf4854b64b5203fc8b719afeec1b2b
Marko Obrovac <mobrovac@wikimedia.org>
2015-12-14 22:44:34 +0100

    Log the request information consistently
    Each request is attached a logger instance, so initialise it with the
    request information that is output with each log entry.
    
    Bug: T121445
    Change-Id: I76519f6d17554b0770fb0df6330495b84875b451

--- app.js
+++ app.js
@@ -96,6 +96,17 @@ function initApp(options) {
         app.conf.spec.paths = {};
     }
 
+    // disable the X-Powered-By header
+    app.set('x-powered-by', false);
+    // disable the ETag header - users should provide them!
+    app.set('etag', false);
+    // enable compression
+    app.use(compression({level: app.conf.compression_level}));
+    // use the JSON body parser
+    app.use(bodyParser.json());
+    // use the application/x-www-form-urlencoded parser
+    app.use(bodyParser.urlencoded({extended: true}));
+
     // set the CORS and CSP headers
     app.all('*', function(req, res, next) {
         if(app.conf.cors !== false) {
@@ -115,16 +126,6 @@ function initApp(options) {
         next();
     });
 
-    // disable the X-Powered-By header
-    app.set('x-powered-by', false);
-    // disable the ETag header - users should provide them!
-    app.set('etag', false);
-    // enable compression
-    app.use(compression({level: app.conf.compression_level}));
-    // use the JSON body parser
-    app.use(bodyParser.json());
-    // use the application/x-www-form-urlencoded parser
-    app.use(bodyParser.urlencoded({extended: true}));
     // serve static files from static/
     app.use('/static', express.static(__dirname + '/static'));
 
