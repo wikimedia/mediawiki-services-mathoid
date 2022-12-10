d48b5b5c710ac365417d776b3a90c4e4f77f14e9
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2022-12-10 09:43:01 +0100

    Update svgo and switch to node v16
    * Adjust to new svgo library according to their manual
    * Update CI config to test with node v16
    * Update production image to node v16
    
    Bug: T311620
    Change-Id: I37eeb83708587ddcabb8906060a98715a1c7c748

--- test/features/math/svgo.js
+++ test/features/math/svgo.js
@@ -9,7 +9,7 @@ const assert = require( '../../utils/assert.js' );
 
 const testcases = [
 	{ in: '<svg>', err: 'Unclosed root tag' },
-	{ in: 'invalid', err: 'Error in parsing SVG' },
+	{ in: 'invalid', err: 'Non-whitespace before first tag' },
 	{
 		in: '<?xml version="1.0"?>\r\n    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="100">\r\n        <text font-size="68" font-weight="bold" font-family="DejaVu Sans"\r\n    y="52" x="4" transform="scale(.8,1.7)"><tspan fill="#248">W3</tspan>C</text>\r\n        <path fill="none" stroke="#490" stroke-width="12" d="m138 66 20 20 30-74"/>\r\n        </svg>',
 		title: 'compress from https://en.wikipedia.org/wiki/File:W3C_valid.svg',
@@ -32,21 +32,19 @@ describe( 'Mathoids SVG compression', () => {
 				}
 			};
 			const data = { svg: t.in };
-			optimize( data, logger )
-				.then( () => {
-					if ( t.err ) {
-						assert.deepEqual( msg, 'warn/svgo' );
-						assert.ok( err.includes( t.err ), `${err} should contain ${t.err}` );
-					} else {
-						assert.deepEqual( msg, false );
-						assert.deepEqual( err, false );
-						assert.ok( data.svg.length <= t.in.length, 'Compression increased the file size' );
-						if ( t.contains ) {
-							assert.ok( data.svg.includes( t.contains ), 'compressed svg should contain', t.contains );
-						}
-					}
-					done();
-				} );
+			optimize( data, logger );
+			if ( t.err ) {
+				assert.deepEqual( msg, 'warn/svgo' );
+				assert.ok( err.includes( t.err ), `${err} should contain ${t.err}` );
+			} else {
+				assert.deepEqual( msg, false );
+				assert.deepEqual( err, false );
+				assert.ok( data.svg.length <= t.in.length, 'Compression increased the file size' );
+				if ( t.contains ) {
+					assert.ok( data.svg.includes( t.contains ), 'compressed svg should contain', t.contains );
+				}
+			}
+			done();
 		} );
 	} );
 } );
